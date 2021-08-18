b205cd5a32a0d55133bf5e84817a3bae1d19400f
ABC10946 <abctech10946@gmail.com>
2021-08-18 23:50:10 +0900

    classのpublic, privateのインデントを修正

--- minesweeper.hpp
+++ minesweeper.hpp
@@ -7,13 +7,13 @@ enum CellType {
 
 
 class Cell {
-        private:
-                int num;
-                CellType type;
-        public:
-                Cell();
-                CellType getType();
-                void setType(CellType type_);
-                int getNum();
-                void setNum(int num_);
+private:
+        int num;
+        CellType type;
+public:
+        Cell();
+        CellType getType();
+        void setType(CellType type_);
+        int getNum();
+        void setNum(int num_);
 };
