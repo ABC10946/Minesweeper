f8c9269e6ce77ad60a561b49dcfe1e7d2a897648
ABC10946 <abctech10946@gmail.com>
2021-08-22 21:31:11 +0900

    プライベート変数の命名規則を変更

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -2,50 +2,50 @@
 #include <vector>
 
 Cell::Cell() {
-    type = EMPTY;
+    _type = EMPTY;
 }
 
-Cell::Cell(CellType type_) {
-    type = type_;
+Cell::Cell(CellType type) {
+    _type = type;
 }
 
 CellType Cell::getType() {
-    return type;
+    return _type;
 }
 
-void Cell::setType(CellType type_) {
-    type = type_;
+void Cell::setType(CellType type) {
+    _type = type;
 }
 
 int Cell::getNum() {
-    return num;
+    return _num;
 }
 
-void Cell::setNum(int num_) {
-    num = num_;
+void Cell::setNum(int num) {
+    _num = num;
 }
 
 
 Field::Field(int width, int height) {
-    field = std::vector<std::vector<Cell>>(height, std::vector<Cell>(width, Cell()));
+    _field = std::vector<std::vector<Cell>>(height, std::vector<Cell>(width, Cell()));
 }
 
 std::vector<std::vector<Cell>> Field::getField() {
-    return field;
+    return _field;
 }
 
-void Field::setField(std::vector<std::vector<Cell>> field_) {
-    field = field_;
+void Field::setField(std::vector<std::vector<Cell>> field) {
+    _field = field;
 }
 
 Cell Field::getCell(int x, int y) {
-    return field.at(y).at(x);
+    return _field.at(y).at(x);
 }
 
 void Field::setCell(int x, int y, Cell cell) {
-    field.at(y).at(x) = cell;
+    _field.at(y).at(x) = cell;
 }
 
 void Field::changeCellType(int x, int y, CellType type) {
-    field.at(y).at(x).setType(type);
+    _field.at(y).at(x).setType(type);
 }
--- minesweeper.h
+++ minesweeper.h
@@ -13,25 +13,25 @@ enum CellType {
 
 class Cell {
  private:
-    int num;
-    CellType type;
+    int _num;
+    CellType _type;
  public:
     Cell();
-    explicit Cell(CellType type_);
+    explicit Cell(CellType type);
     CellType getType();
-    void setType(CellType type_);
+    void setType(CellType type);
     int getNum();
-    void setNum(int num_);
+    void setNum(int num);
 };
 
 
 class Field {
  private:
-    std::vector<std::vector<Cell>> field;
+    std::vector<std::vector<Cell>> _field;
  public:
     Field(int width, int height);
     std::vector<std::vector<Cell>> getField();
-    void setField(std::vector<std::vector<Cell>> field_);
+    void setField(std::vector<std::vector<Cell>> field);
     Cell getCell(int x, int y);
     void setCell(int x, int y, Cell cell);
     void changeCellType(int x, int y, CellType type);
