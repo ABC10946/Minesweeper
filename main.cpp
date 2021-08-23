6f88b1d9dba859a2edbaca7bdd92a1e3165a248f
ABC10946 <abctech10946@gmail.com>
2021-08-23 23:39:08 +0900

    フィールド情報を外部から入力できるように変更

--- main.cpp
+++ main.cpp
@@ -3,18 +3,38 @@
 #include "minesweeper.h"
 
 int main(int argc, char *argv[]) {
-    int width = 30;
-    int height = 30;
+    /* input process */
+    int width, height;
+    int line_num;
 
+    std::cin >> width >> height;
     Field field = Field(width, height);
 
-    field.setCell(0, 0, Cell(BOMB));
-    field.changeCellType(0, 2, NUM);
-    std::cout << "This is minesweeper" << std::endl;
+    std::cin >> line_num;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            std::cout << field.getCell(x, y).getType();
+    int bomb_x, bomb_y;
+    for (int num=0; num < line_num; num ++) {
+        std::cin >> bomb_x >> bomb_y;
+        field.setCell(bomb_x, bomb_y, Cell(BOMB));
+    }
+    /* input process */
+
+
+    for (int y = 1; y < height-1; y++) {
+        for (int x = 1; x < width-1; x++) {
+            field.setNumCell(x, y);
+        }
+    }
+
+    for (int y = 1; y < height-1; y++) {
+        for (int x = 1; x < width-1; x++) {
+            if (field.getCell(x, y).getType() == NUM) {
+                std::cout << field.getCell(x, y).getNum();
+            } else if (field.getCell(x, y).getType() == BOMB) {
+                std::cout << "B";
+            } else if (field.getCell(x, y).getType() == EMPTY) {
+                std::cout << " ";
+            }
         }
         std::cout << std::endl;
     }
